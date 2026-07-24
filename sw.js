1f93f664b8c14ba516ca5f30f986bb7ec04a2548
nastybutfancy <fac45card@gmail.com>
2026-07-24 14:44:59 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,54 @@
-const CACHE="movievault-v1";const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest"];self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE = "movievault-v2";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", function (event) {
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches
+      .open(CACHE)
+      .then(function (cache) {
+        return cache.addAll(ASSETS);
+      })
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches
+      .keys()
+      .then(function (keys) {
+        return Promise.all(
+          keys
+            .filter(function (key) {
+              return key !== CACHE;
+            })
+            .map(function (key) {
+              return caches.delete(key);
+            })
+        );
+      })
+      .then(function () {
+        return self.clients.claim();
+      })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    fetch(event.request)
+      .then(function (response) {
+        return response;
+      })
+      .catch(function () {
+        return caches.match(event.request);
+      })
+  );
+});
